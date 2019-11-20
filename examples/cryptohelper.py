e54e999ead0aa56bae63d3decabe423020166273
Anil Daoud <anil@via.ecp.fr>
2019-11-20 21:34:55 +0800

    add slack bot example and upgrade telegram api

--- examples/cryptohelper.py
+++ examples/cryptohelper.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+import slack
 from telegram import InlineQueryResultArticle,InputTextMessageContent
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler, Filters, InlineQueryHandler
@@ -8,13 +10,18 @@ import logging
 import exchanges
 import requests
 import math
+from datetime import datetime, timedelta
+import re
+import time
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s-%(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 BOTNAME = '@BotName_Bot'
-TOKEN = 'TOKEN'
+TOKEN = 'TELEGRAMTOKEN'
 CL_API_KEY = 'CLAPIKEY'
+SLACK_BOT_TOKEN = "SLACKTOKEN"
+SLACK_MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
 millnames = ['',' k',' mio',' bio',' trillion']
 
@@ -28,23 +35,30 @@ def millify(n):
     millidx = max(0,min(len(millnames)-1, int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
     return '{:.1f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
 
-def startMsg(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Type /list to get a list of supported exchanges and underlyings.\nType /price <underlying> <exchange> to retrieve a price.\nFor example /price btcusd kraken")
+def startMsg(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Type /list to get a list of supported exchanges and underlyings.\nType /price <underlying> <exchange> to retrieve a price.\nFor example /price btcusd kraken")
 
-def bitmexMsg(bot, update):
+def bitmexMsg(update, context):
     logger.info("Received /bitmex command from %s" % update.effective_user)
     if update.message.text.split().__len__()<=1:
-        bot.send_message(chat_id=update.message.chat_id, text="Syntax is /bitmex <instrument> <expiry>. For example /bitmex XBT H18")
+        context.bot.send_message(chat_id=update.message.chat_id, text="Syntax is /bitmex <future>. For example /bitmex XBTH18")
     else:
-        fut = update.message.text.split()[1].upper()
-        update.message.reply_text(bitmex(fut))
+        futlist  = update.message.text.upper().split()[1:]
+        message_text = ""
+        for i, fut in enumerate(futlist):
+            message_text+=bitmex(fut)
+            if i != len(futlist)-1:
+                message_text+='\n------------\n'
+        update.message.reply_text(message_text)
 
 def bitmex(fut):
+    if len(fut) == 3:
+        fut = 'XBT'+fut
     if fut[:3] == 'XBT':
         exch = ['gdax', 'bitstamp']
         spot = 'BTCUSD'
     else:
-        exch = ['poloniex']
+        exch = ['poloniex','bittrex']
         spot = fut[:3] + 'BTC'
     fut_stream = bm.get_stream(fut)
     if fut_stream == None:
@@ -52,16 +66,24 @@ def bitmex(fut):
     fut_bid = bm.get_quote(fut, 'bid')
     fut_ask = bm.get_quote(fut, 'ask')
     if fut_bid == None or fut_ask == None:
-        message = "Something went wrong, check future code"
+        message = "Something went wrong, check future code " + fut
         return message
+    expiry = bm.get_instrument(fut)['expiry']
+    nowdate = datetime.now()
+    if expiry is None:
+        expdate = nowdate
+    else:
+        expdate = datetime.strptime(bm.get_instrument(fut)['expiry'],'%Y-%m-%dT%H:%M:%S.%fZ')
+    yearstoexp = (expdate - nowdate).total_seconds()/3600/24/365.25
     spot_bid = 0
     spot_ask = 0
     i = 0
     for en in exch:
         e = exchanges.get_exchange(en)
-        spot_bid += e.get_quote(spot, 'bid')
-        spot_ask += e.get_quote(spot, 'ask')
-        i+=1
+        if spot in e.get_supported_underlyings():
+            spot_bid += e.get_quote(spot, 'bid')
+            spot_ask += e.get_quote(spot, 'ask')
+            i+=1
     spot_bid = float(spot_bid / i)
     spot_ask = float(spot_ask / i)
     basis_bid = fut_bid - spot_ask
@@ -69,25 +91,35 @@ def bitmex(fut):
     spot_mid = (spot_bid + spot_ask)/2
     fut_mid = (fut_ask + fut_bid)/2
     basis_mid = (basis_ask + basis_bid)/2
+    message = ""
     if basis_ask < 0:
         basis_premium = 100 * basis_ask / spot_mid
-    else:
+    elif spot_mid != 0:
         basis_premium = 100 * basis_bid / spot_mid
+    else:
+        basis_premium = 0
+        message += "Couldn't find supported exchange for spot price.\n"
+    if yearstoexp == 0:
+        annualized_premium = 0
+    else:
+        annualized_premium = 100*((1+basis_premium/100)**(1/yearstoexp)-1)
     if spot_bid > 1:
-        message = "Spot %s price is %.2f: %.2f / %.2f" % (spot, spot_mid, spot_bid, spot_ask) + '\n'
+        message += "Spot %s price is %.2f: %.2f / %.2f" % (spot, spot_mid, spot_bid, spot_ask) + '\n'
         message += "Fut %s price is %.2f: %.2f / %.2f" % (fut, fut_mid, fut_bid,fut_ask) + '\n'
         message += "Basis is %.2f: %.2f / %.2f. Premium of %.1f%%" % (basis_mid, basis_bid, basis_ask, basis_premium)
     else:
-        message = "Spot %s price is %.4g: %.4g / %.4g" % (spot, spot_mid, spot_bid, spot_ask) + '\n'
+        message += "Spot %s price is %.4g: %.4g / %.4g" % (spot, spot_mid, spot_bid, spot_ask) + '\n'
         message += "Fut %s price is %.4g: %.4g / %.4g" % (fut,fut_mid, fut_bid, fut_ask) + '\n'
         message += "Basis is %.4g: %.4g / %.4g. Premium of %.1f%%" % (basis_mid, basis_bid, basis_ask, basis_premium)
+    if annualized_premium !=0:
+        message += " (%.1f%% annualised)" % annualized_premium
     return message
 
-def unknownMsg(bot, update):
+def unknownMsg(update, context):
     logger.info("Received unknown command from %s: %s" % (update.effective_user, update.message.text))
-    bot.send_message(chat_id=update.message.chat_id, text="Sorry, try /start again?")
+    context.bot.send_message(chat_id=update.message.chat_id, text="Sorry, try /start again?")
 
-def listExchangesMsg(bot, update):
+def listExchangesMsg(update, context):
     logger.info("Received /list command from %s" % update.effective_user)
     update.message.reply_text(list_text())
 
@@ -97,7 +129,7 @@ def list_text():
     end_text = 'Try /exchange <exchange> to price all underlyings availablle on an exchange'
     return exchange_text + '\n\n' + underlyings_text + '\n\n' + end_text
 
-def summaryMsg(bot, update):
+def summaryMsg(update, context):
     logger.info("Received /summary command from %s" % update.effective_user)
     logger.info("Command content: %s" % update.message.text)
     if update.message.text.split().__len__() <= 1:
@@ -126,6 +158,8 @@ def summary(ccy_list):
         'bch' : 'bitcoin-cash',
         'cash' : 'cash-poker-pro',
         'zec' : 'zcash',
+        'lgo' : 'legolas-exchange',
+        'legolas' : 'legolas-exchange',
         'xlm' : 'stellar',
         'zrx' : '0x'
     }
@@ -179,7 +213,7 @@ def summary(ccy_list):
             results.append('')
     return results
 
-def exchangeMsg(bot, update):
+def exchangeMsg(update, context):
     logger.info("Received /exchange command from %s" % update.effective_user)
     logger.info("Command content: %s" % update.message.text)
     if update.message.text.split().__len__() <= 1:
@@ -292,20 +326,27 @@ def fx(underlying, exchange, cross_ccy):
         else:
             return ["Exchange %s does not support %s and %s" % (exchange, FORPAIR, DOMPAIR)]
     results = []
-    if cross_ccy in ('USD','EUR','JPY'):
+    if not intersectExchanges:
+        return ["No exchange supports %s and %s" % (FORPAIR, DOMPAIR)]
+
+    try:
         e = exchanges.get_exchange(intersectExchanges[0].lower())
         fxRate = float(e.get_quote(underlying,'last'))
         results.append('%s rate for %s is %.5g' % (intersectExchanges[0], underlying, fxRate))
-    else:
+    except:
         url = 'http://www.apilayer.net/api/live?access_key='+CL_API_KEY+'&currencies='+FORCCY+','+DOMCCY
         r = requests.get(url)
         r.raise_for_status()
         j = r.json()
-        if j['success']:
-            domOfficialRate = r.json()['quotes']['USD'+DOMCCY]
-            forOfficialRate = r.json()['quotes']['USD'+FORCCY]
-            fxRate = domOfficialRate / forOfficialRate
-            results.append('Currency Layer %s FX rate is %.5g' % (underlying, fxRate))
+        if j['success'] and 'quotes'in j:
+            if 'USD'+DOMCCY in j['quotes'] and 'USD'+FORCCY in j['quotes']:
+                domOfficialRate = j['quotes']['USD'+DOMCCY]
+                forOfficialRate = j['quotes']['USD'+FORCCY]
+                fxRate = domOfficialRate / forOfficialRate
+                results.append('Currency Layer %s FX rate is %.5g' % (underlying, fxRate))
+            else:
+                fxRate = 0
+                results.append('Could not retrieve %s FX rate from CurrencyLayer' % underlying)
         else:
             fxRate = 0
             results.append('Could not retrieve %s FX rate from CurrencyLayer' % underlying)
@@ -329,7 +370,7 @@ def fx(underlying, exchange, cross_ccy):
             results.append('%s: one of the quotes is worth 0' % exch)
     return results
 
-def priceMsg(bot, update):
+def priceMsg(update, context):
     logger.info("Received /price command from %s" % update.effective_user)
     logger.info("Command content: %s" % update.message.text)
     if update.message.text.split().__len__() <= 2:
@@ -338,12 +379,11 @@ def priceMsg(bot, update):
     underlying = update.message.text.split()[1].upper()
     exchange_list = update.message.text.split()[2:]
     if exchange_list == []:
-        update.message.reply_text('No exchange support underlying %s' % underlying)
-        return
+        exchange_list = ['all']
     messages = price(underlying, exchange_list)
     update.message.reply_text('\n'.join(messages))
 
-def fxMsg(bot, update):
+def fxMsg(update, context):
     logger.info("Received /fx command from %s" % update.effective_user)
     logger.info("Command content: %s" % update.message.text)
     if update.message.text.split().__len__() <= 2:
@@ -364,7 +404,7 @@ def fxMsg(bot, update):
     messages = fx(underlying, exchange_list, cross_ccy)
     update.message.reply_text('\n'.join(messages))
 
-def inline_query(bot, update):
+def inline_query(update, context):
     query = update.inline_query.query
     if not query:
         return
@@ -388,12 +428,12 @@ def inline_query(bot, update):
     results.append(InlineQueryResultArticle(id=uuid4(), title='Enter to display results', input_message_content=InputTextMessageContent(reply_text)))
     bot.answer_inline_query(update.inline_query.id, results)
 
-def error(bot, update, error):
-    logger.warn('Update "%s" caused error "%s"' % (update, error))
+def error(update, context):
+    logger.warn('Update "%s" caused error "%s"' % (update, context.error))
 
-def main():
+def main_telegram():
     # Create the EventHandler and pass it to the bot's token
-    updater = Updater(token=TOKEN)
+    updater = Updater(token=TOKEN, use_context=True)
 
     # Get the dispathcher to register handlers
     dispatcher = updater.dispatcher
@@ -432,6 +472,87 @@ def main():
     updater.idle()
     logger.info("Exiting bot.")
 
+def slack_parse_bot_commands(bot_id, slack_events):
+    for event in slack_events:
+        if event["type"] == "message" and not "subtype" in event:
+            matches = re.search(SLACK_MENTION_REGEX, event["text"])
+            user_id, message = (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+            if user_id == bot_id:
+                return message, event["channel"]
+    return None, None
+
+def slack_handle_command(client, command, channel):
+    default_response = "Not sure what you mean. Try:\n  *summary* <list of tokens>\n  *fx* <FORDOM> <exchange> <CROSS>\n  *bitmex* <list of futures>\n  *price* <ticker> <list of exchanges>"
+    response = None
+    if command.lower().startswith("summary"):
+        if command.split().__len__() <= 1:
+            ccy = ['globalsummary']
+        else:
+            ccy = command.split()[1:]
+        response = '\n'.join(summary(ccy))
+    elif command.lower().startswith("fx"):
+        if command.split().__len__() <= 2:
+            response = 'Syntax is "*fx* <fxpair> <exchange> <cross>".\nTry "*fx* <fxpair> all eth" to price your underlying on all supported exchanges with eth as the cross ccy.'
+        else:
+            underlying = command.split()[1].upper()
+            exchange_list = command.split()[2]
+            if command.split().__len__() >= 4:
+                cross_ccy = command.split()[3].upper()
+            else:
+                if underlying[-3:].upper() == 'BTC' or underlying[:3].upper() == 'BTC':
+                    cross_ccy = 'USD'
+                else:
+                    cross_ccy = 'BTC'
+            if exchange_list == []:
+                response = 'No exchange specified. Try all or gdax for example'
+            else:
+                messages = fx(underlying, exchange_list, cross_ccy)
+                response = '\n'.join(messages)
+    elif command.lower().startswith("bitmex"):
+        if command.split().__len__()<=1:
+            response = "Syntax is *bitmex* <future>. For example *bitmex* XBTM18"
+        else:
+            futlist  = command.upper().split()[1:]
+            message_text = ""
+            for i, fut in enumerate(futlist):
+                message_text+=bitmex(fut)
+                if i != len(futlist)-1:
+                    message_text+='\n------------\n'
+            response = message_text
+    elif command.lower().startswith("price"):
+        if command.split().__len__() <= 2:
+            response = 'Syntax is "*price* <underlying> <exchange1> <exchange2> <...>".\nTry "*price* <underlying> all" to price your underlying on all supported exchanges.'
+        else:
+            underlying = command.split()[1].upper()
+            exchange_list = command.split()[2:]
+            if exchange_list == []:
+                exchange_list = ['all']
+            messages = price(underlying, exchange_list)
+            response = '\n'.join(messages)
+    client.api_call("chat.postMessage",channel=channel,text=response or default_response)
+
+def main_slack():
+    # instantiate Slack client
+    slack_client = slack.WebClient(SLACK_BOT_TOKEN)
+    slack_bot_id = None
+    # Constants
+    RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+
+    if slack_client.rtm_connect(with_team_state=False, auto_reconnect=True):
+        logger.info("Slack Bot connected and running!")
+        # Read bot's user ID by calling Web API method `auth.test`
+        slack_bot_id = slack_client.api_call("auth.test")["user_id"]
+        while slack_client.server.connected is True:
+            command, channel = slack_parse_bot_commands(slack_bot_id, slack_client.rtm_read())
+            if command:
+                slack_handle_command(slack_client, command, channel)
+            time.sleep(RTM_READ_DELAY)
+    else:
+        logger.error("Connection failed. Exception traceback printed above")
+
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1 and sys.argv[1].lower() == 'slack':
+        main_slack()
+    else:
+        main_telegram()
 
